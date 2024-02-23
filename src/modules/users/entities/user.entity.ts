b225c5f2ba04a73331b1f0d5a9cca51308ee23af
DuyChamHoc <duytran9262002@gmail.com>
2024-02-23 17:31:06 +0700

    aa

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,8 +1,6 @@
 import Container from 'typedi'
 import { Column, Entity, Generated, Index } from 'typeorm'
 import { AppBaseEntity } from '../../../base/base.entity'
-import { execProc, Procs } from '../../../database/procs'
-import { CacheKeys, CacheManager } from '../../../utils/cache'
 import { Errors } from '../../../utils/error'
 import { UserDTO } from '../dtos/user.dto'
 
@@ -21,22 +19,4 @@ export class User extends AppBaseEntity {
     @Index()
     @Column({ length: 6 })
     salt: string
-
-    static async getProfile(userId: string) {
-        const cacheManager = Container.get(CacheManager)
-        const cachedUser = await cacheManager.getObject(
-            UserDTO,
-            CacheKeys.user(userId)
-        )
-        if (cachedUser) {
-            return cachedUser
-        }
-
-        const users = await execProc(UserDTO, Procs.UserGetProfile, [userId])
-        if (users[0]) {
-            await cacheManager.setObject(CacheKeys.user(userId), users[0])
-            return users[0]
-        }
-        throw Errors.UserNotFound
-    }
 }
